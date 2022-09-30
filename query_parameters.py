1821b02ddae6df4c668d479d7c324fa2035fbe57
LauPozzi <laura3.pozzi@mail.polimi.it>
2022-09-30 16:32:10 +0200

    fix query par

--- query_parameters.py
+++ query_parameters.py
@@ -5,6 +5,5 @@ def query_param(query):
     link1 = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term={}&retmode=json&RetMax=100&WebEnv=%3Cwebenv%20string%3E&usehistory=y".format(newString)
     f1 = requests.get(link1)
     dict = f1.json()
-    dict2 = dict['esearchresult']
-    return dict2['webenv'], dict2['querykey']
+    return dict['esearchresult']['webenv'], dict['esearchresult']['querykey']
 
