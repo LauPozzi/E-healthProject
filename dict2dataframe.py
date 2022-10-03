482cb145ff97cb46c3918d8c4ea179ff5e90383e
LauPozzi <laura3.pozzi@mail.polimi.it>
2022-10-03 11:12:30 +0200

    function name changed

--- dict2dataframe.py
+++ dict2dataframe.py
@@ -1,32 +1,78 @@
 import collections.abc
 import datetime
-import sys
-import time
-import functools
-import operator
-import traceback
-
 import pandas as pd
 import requests
 import xmltodict
 
+# TODO: set the default values, if it's not the last get() a dictionary should be provided to avoid errors (as in
+#  the years-months-days extractions)
 
-def extract_authors(authors):
+def extract_titles(article_list):
+    return list(map(lambda d: d.get('MedlineCitation').get('Article').get('ArticleTitle', "n.a."), article_list))
+
+def extract_dates(article_list):
+    years = list(map(int, map(lambda d: d.get('MedlineCitation', None).get('Article', None).get('ArticleDate', {
+        'Year': datetime.MINYEAR}).get('Year', datetime.MINYEAR), article_list)))
+
+    months = list(map(int, map(lambda d: d.get('MedlineCitation', None).get('Article', None).get('ArticleDate', {
+        'Month': 1}).get('Month', 1), article_list)))
+
+    days = list(map(int, map(lambda d: d.get('MedlineCitation', None).get('Article', None).get('ArticleDate', {
+        'Day': 1}).get('Day', 1), article_list)))
+
+    return [datetime.datetime(y, m, d) for y, m, d in zip(years, months, days)]
+
+def extract_authors(article_list):
     authors_out = []
+    authors = list(
+        map(lambda d: d.get('MedlineCitation').get('Article').get('AuthorList', {'Author': []}).get('Author'),
+            article_list))
     for author in authors:
-        authors_out.append([])
         if isinstance(author, list):
-            authors_out[-1] = list(map(lambda d: d.get('LastName', 'n.a.') + ' ' + d.get('ForeName', 'n.a.'), author))
+            authors_out.append(list(map(lambda d: d.get('LastName', 'n.a.') + ' ' + d.get('ForeName', 'n.a.'), author)))
         elif isinstance(author, collections.abc.Mapping):
-            authors_out[-1] = [author.get('LastName', 'n.a.') + ' ' + author.get('ForeName', 'n.a.')]
+            authors_out.append([author.get('LastName', 'n.a.') + ' ' + author.get('ForeName', 'n.a.')])
         else:
-            authors_out[-1] = ['n.a.']
-
+            authors_out.append(['n.a.'])
     return authors_out
 
+def extract_journal_names(article_list):
+    return list(map(lambda d: d.get('MedlineCitation').get('Article').get('Journal').get('Title'), article_list))
 
-def extract_abstracts(abstracts):
+def extract_studytypes(article_list):
+    study_types_out = []
+    study_types = list(map(lambda d: d.get('MedlineCitation').get('Article').get('PublicationTypeList').get('PublicationType'),article_list))
+    for type in study_types:
+        if isinstance(type, list):
+            study_types_out.append(list(map(lambda d: d.get('#text', 'n.a.'), type)))
+        elif isinstance(type, collections.abc.Mapping):
+            study_types_out.append([type.get('#text', 'n.a.')])
+        else:
+            study_types_out.append(['n.a.'])
+
+    return study_types_out
+def extract_keywords(article_list):
+    keywords_dicts = list(
+        map(lambda d: d.get('MedlineCitation').get('KeywordList', {'Keyword': [{'#text': 'n.a.'}]}).get('Keyword'),
+            article_list))
+    keywords=[]
+    for keyword in keywords_dicts:
+        keywords.append(list(map(lambda k: k.get('#text'), keyword)))
+    return keywords
+
+def extract_dois(article_list):
+    DOI_std = "https://doi.org/"
+    dois = list(map(lambda d: list(
+        filter(lambda k: k['@IdType'] == 'doi', d.get('PubmedData').get('ArticleIdList').get('ArticleId'))),
+                    article_list))
+    dois = list(map(lambda d: d[0], dois))  # To obtain the list of dictionaries
+    dois = list(map(lambda d: DOI_std + d.get('#text'), dois))
+    return dois
+
+def extract_abstracts(article_list):
     abstracts_out = []
+    abstracts = list(
+        map(lambda d: d.get('MedlineCitation').get('Article').get('Abstract').get('AbstractText'), article_list))
     for abstract in abstracts:
         if isinstance(abstract, str):
             abstracts_out.append(abstract)
@@ -46,79 +92,35 @@ def dict_2_dataframe(article_set: dict):
     :return:
     :rtype:
     """
-    dataframe = pd.DataFrame()
-    column_names = ['Article Title', 'Date', 'Authors', 'Journal', 'Keywords', 'DOI', 'Abstract']
-    data = []
-
     article_list = article_set['PubmedArticle']
 
-    # TODO: set the default values, if it's not the last get() a dictionary should be provided to avoid errors (as in
-    #  the years-months-days extractions)
+    data_dict = {'Article Title': extract_titles(article_list),
+                 'Date': extract_dates(article_list),
+                 'Authors': extract_authors(article_list),
+                 'Journal': extract_journal_names(article_list),
+                 'Study Type': extract_studytypes(article_list),   
+                 'Keywords': extract_keywords(article_list),
+                 'DOI': extract_dois(article_list),
+                 'Abstract': extract_abstracts(article_list)
+                 }
+    return pd.DataFrame(data_dict)
 
-    titles = list(map(lambda d: d.get('MedlineCitation').get('Article').get('ArticleTitle', "n.a."), article_list))
+    # QUESTO CONTROLLO VIENE FATTO AUTOMATICAMENTE NELLA CREAZIONE DEL DATAFRAME, QUINDI NON LO METTEREI
+    # try:
+    #     for datum in data:
+    #         assert len(datum) == len(data[0])
+    # except AssertionError:
+    #     _, _, tb = sys.exc_info()
+    #     traceback.print_tb(tb)  # Fixed format
+    #     tb_info = traceback.extract_tb(tb)
+    #     filename, line, func, text = tb_info[-1]
+    #
+    #     print(
+    #         'An error occurred on line {} in statement {}.\nThe data extracted are not of the same length'.format(line,
+    #                                                                                                               text))
+    #     exit(1)
 
-    years = list(map(int, map(lambda d: d.get('MedlineCitation', None).get('Article', None).get('ArticleDate', {
-        'Year': datetime.MINYEAR}).get('Year', datetime.MINYEAR), article_list)))
 
-    months = list(map(int, map(lambda d: d.get('MedlineCitation', None).get('Article', None).get('ArticleDate', {
-        'Month': 1}).get('Month', 1), article_list)))
-
-    days = list(map(int, map(lambda d: d.get('MedlineCitation', None).get('Article', None).get('ArticleDate', {
-        'Day': 1}).get('Day', 1), article_list)))
-
-    dates = [datetime.datetime(y, m, d) for y, m, d in zip(years, months, days)]
-
-    authors = list(
-        map(lambda d: d.get('MedlineCitation').get('Article').get('AuthorList', {'Author': []}).get('Author'),
-            article_list))
-    authors = extract_authors(authors)
-
-    journals = list(map(lambda d: d.get('MedlineCitation').get('Article').get('Journal').get('Title'), article_list))
-
-    keywords = list(
-        map(lambda d: d.get('MedlineCitation').get('KeywordList', {'Keyword': [{'#text': 'n.a.'}]}).get('Keyword'),
-            article_list))
-    for i, keyword in enumerate(keywords):
-        keywords[i] = list(map(lambda k: k.get('#text'), keyword))
-
-    DOI_std = "https://doi.org/"
-    dois = list(map(lambda d: list(
-        filter(lambda k: k['@IdType'] == 'doi', d.get('PubmedData').get('ArticleIdList').get('ArticleId'))),
-                    article_list))
-    dois = list(map(lambda d: d[0], dois))  # To obtain the list of dictionaries
-    dois = list(map(lambda d: DOI_std + d.get('#text'), dois))
-
-    abstracts = list(
-        map(lambda d: d.get('MedlineCitation').get('Article').get('Abstract').get('AbstractText'), article_list))
-    abstracts = extract_abstracts(abstracts)
-
-    data.append(titles)
-    data.append(dates)
-    data.append(authors)
-    data.append(journals)
-    data.append(keywords)
-    data.append(dois)
-    data.append(abstracts)
-
-    try:
-        for datum in data:
-            assert len(datum) == len(data[0])
-    except AssertionError:
-        _, _, tb = sys.exc_info()
-        traceback.print_tb(tb)  # Fixed format
-        tb_info = traceback.extract_tb(tb)
-        filename, line, func, text = tb_info[-1]
-
-        print(
-            'An error occurred on line {} in statement {}.\nThe data extracted are not of the same length'.format(line,
-                                                                                                                  text))
-        exit(1)
-
-    for name in column_names:
-        index = column_names.index(name)
-        dataframe.insert(loc=index, column=name, value=data[index])
-
-    return dataframe
 
 
 if __name__ == '__main__':
@@ -142,4 +144,4 @@ if __name__ == '__main__':
     # TODO: study type not in xml... Get from abstract?
     database = dict_2_dataframe(ArticleSet)
 
-    print(database['Article Title'])
+    print(database['Study Type'])
