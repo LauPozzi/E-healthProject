0e9ec13822e91d27bdb2a45b0fc42ad50a8a0448
DianaNigrisoli <diana.nigrisoli@mail.polimi.it>
2022-10-10 18:46:35 +0200

    code for counting recurrent words in abstract

---
+++ algorithm_classification.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from main import main
+import string
+
+def count_words(text, blacklist_words):
+    #code taken from: https://www.geeksforgeeks.org/python-count-occurrences-of-each-word-in-given-text-file/
+
+    d = dict()
+
+    # Remove the leading spaces and newline character
+    line = text.strip()
+
+    # Convert the characters in line to
+    # lowercase to avoid case mismatch
+    line = line.lower()
+
+    # Remove the punctuation marks from the line
+    line = line.translate(line.maketrans("", "", string.punctuation))
+
+    # Split the line into words
+    words = line.split()
+
+    # Iterate over each word in line
+    for word in words:
+        # Check if the word is already in dictionary
+        if word in d:
+        # Increment count of word by 1
+            d[word] = d[word] + 1
+        else:
+        # Add the word to dictionary with count 1
+            if word in blacklist_words:
+                pass
+            else:
+                d[word] = 1
+
+    return d
+
+
+
+def classification_alg():
+    df = main()
+
+    wordlist = [dict() for x in range(df.shape[0])]
+
+    #Step1 - count occurences of all words (minus black list)
+    blacklist_dict = pd.read_csv('blacklist_dict.csv')
+    blacklist_dict.head(3)
+    blacklist = blacklist_dict['WORD'].tolist()
+
+    for i in range(df.shape[0]):
+        wordlist[i] = count_words(df.iloc[i]['Abstract'], blacklist)
+
+    print('hello')
+
+    #TODO: Step2 - create a dictionary based on a treshold
+
+
+    #TODO: Step3 - scale the occurencies of the words in the dictionary in [0.06, 1]
+
+
+    #TODO: Step4 - on each abstract compute the score and scale it in [0-1]
+
+
+    #TODO: Step5 - scaled score > 0.09 --> classify as 1
+
+
+
+
+
+
+if __name__ == '__main__':
+    classification_alg()
