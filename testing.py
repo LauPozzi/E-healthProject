1a0cf46995815c28e87607c24bf52140dd72d0b4
RaffaeleBerzoini <raffaele.berzoini@mail.polimi.it>
2022-10-18 20:19:53 +0200

    validation and testing graphs

--- testing.py
+++ testing.py
@@ -2,8 +2,10 @@ from algorithm_classification import THRESHOLD
 from validation import get_auc
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sn
 
-
+#TODO: documentation
 def join_dataframes(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     merged_dataframe = pd.merge(df1, df2, on=['Article Title', 'Topic of interest'], how='inner')
     return merged_dataframe
@@ -15,9 +17,15 @@ if __name__ == '__main__':
 
     merged_df = join_dataframes(alg_scores, gold_std)
 
-    ACCs, TPRs, FPRs, SPECs, thresholds = get_auc(merged_df.loc[:, ['Manual Classification']], merged_df.loc[:, ['Score']])
+    ACCs, TPRs, FPRs, SPECs, thresholds, CMs = get_auc(merged_df.loc[:, ['Manual Classification']], merged_df.loc[:, ['Score']])
 
     index = np.where(thresholds == THRESHOLD)[0][0]
     print("\nThe accuracy with the set THRESHOLD of ", round(THRESHOLD, 4), " is ", round(ACCs[index], 4))
+    print("Specificity with the set THRESHOLD of ", round(THRESHOLD, 4), "): ", round(SPECs[index], 4))
+    print("Sensitivity with the set THRESHOLD of ", round(THRESHOLD, 4), "): ", round(TPRs[index], 4))
 
-    # get_auc(gold_std, alg_scores)
+    plt.figure()
+    sn.heatmap(CMs[index], annot=True, annot_kws={"size": 16}, cbar=False, cmap='RdYlGn')
+    plt.ylabel('Ground truth')
+    plt.xlabel('Algorithm classification')
+    plt.show()
--- validation.py
+++ validation.py
@@ -3,8 +3,9 @@ import numpy as np
 from algorithm_classification import matching_articles
 from sklearn.metrics import confusion_matrix, auc
 import matplotlib.pyplot as plt
+import seaborn as sn
 
-
+#TODO: documentation
 def get_auc(gold_standard: pd.DataFrame, alg_scoring: pd.DataFrame, graph=True):
     if not gold_standard.shape[0] == alg_scoring.shape[0]:
         raise Exception("Dataframes are not of the same length. Check your inputs")
@@ -16,15 +17,16 @@ def get_auc(gold_standard: pd.DataFrame, alg_scoring: pd.DataFrame, graph=True):
     TPRs = []
     ACCs = []
     SPECs = []
+    CMs = []
     # +0.1 to assure last results contains all 0s (due to >= in matching_articles())
     thresholds = np.linspace(min(algorithm_scores), max(algorithm_scores) + 0.001, 1000)
     for threshold in thresholds:
         algorithm_classification = matching_articles(algorithm_scores, threshold)
         cm = confusion_matrix(manual_classifications, algorithm_classification)
-        TP = cm[0][0]
-        FN = cm[0][1]
-        FP = cm[1][0]
-        TN = cm[1][1]
+        TN = cm[0][0]
+        FP = cm[0][1]
+        FN = cm[1][0]
+        TP = cm[1][1]
 
         # Fall out or false positive rate
         FPR = FP / (FP + TN)
@@ -37,9 +39,13 @@ def get_auc(gold_standard: pd.DataFrame, alg_scoring: pd.DataFrame, graph=True):
         TPRs.append(TPR)
         SPECs.append(1-FPR)
         ACCs.append(ACC)
+        CMs.append(cm)
 
+    index = ACCs.index(max(ACCs))
     print("The AUC score is: ", round(auc(FPRs, TPRs), 4))
-    print("Best accuracy of ", round(max(ACCs), 4), " with THRESHOLD of ", round(thresholds[ACCs.index(max(ACCs))], 4))
+    print("Best accuracy of ", round(max(ACCs), 4), " with threshold of ", round(thresholds[index], 4))
+    print("Specificity (threshold = ", round(thresholds[index], 4), "): ", round(SPECs[index], 4))
+    print("Sensitivity (threshold = ", round(thresholds[index], 4), "): ", round(TPRs[index], 4))
 
     if graph:
         plt.figure()
@@ -57,15 +63,20 @@ def get_auc(gold_standard: pd.DataFrame, alg_scoring: pd.DataFrame, graph=True):
         plt.plot(thresholds, TPRs)
         plt.plot(thresholds, SPECs)
         marker_size = (plt.rcParams['lines.markersize'] ** 2)*4
-        plt.scatter(thresholds[ACCs.index(max(ACCs))], max(ACCs), c='#ff7f0e', marker='X', s=marker_size)
+        plt.scatter(thresholds[index], max(ACCs), c='#ff7f0e', marker='X', s=marker_size)
         plt.legend(['Accuracy', 'Sensitivity', 'Specificity'])
         plt.xlabel('Threshold')
         plt.ylabel('%')
         plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.0])
         plt.show()
+        plt.figure()
+        sn.heatmap(CMs[index], annot=True, annot_kws={"size": 16}, cbar=False, cmap='RdYlGn')
+        plt.ylabel('Ground truth')
+        plt.xlabel('Algorithm classification')
+        plt.show()
 
-    return ACCs, TPRs, FPRs, SPECs, thresholds
+    return ACCs, TPRs, FPRs, SPECs, thresholds, CMs
 
 
 if __name__ == '__main__':
